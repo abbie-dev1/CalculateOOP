ddfd6dcf3d0e24b7ec966887626aa2238e1256af
Admin <Admin@LON-CL1>
2023-10-17 23:52:22 -0700

    Add project files.

---
+++ CalculateOOP/Calculator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateOOP
+{
+    public class Calculator
+    {
+        //Data members(inputs)
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+
+        //Construtor
+        public Calculator() 
+        {
+            num1 = 0;
+            num2 = 0;
+        }
+
+        //Overload Method
+        public Calculator(int x, int y)
+        {
+            num1 = x;
+            num2 = y;
+        }
+
+        //Calculation methods
+        public int calculateSum()
+        {
+            return num1 + num2;
+        }
+        public int calculateDiff()
+        {
+            return num1 - num2;
+        }
+        public int calculateMulti()
+        {
+            return num1 * num2;
+        }
+        public int calculateDivide()
+        {
+            int divide = 0;
+            try
+            {
+                divide = num1 / num2;
+            }
+            catch(Exception ) 
+            { 
+                Console.WriteLine("You cannot divide by zero");
+            }
+            return divide;
+        }
+    }
+}
---
+++ CalculateOOP/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare variables
+            //This program will perform arithmetic calculations
+
+            int num1, num2, num3, result = 0;
+            char operation;
+
+            //Assign values 
+            Console.WriteLine("Please enter first number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter second number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter third number: ");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the following for multiple purpose:  " +
+                "\n + for addition \n - for subtraction \n * for multiplication \n / for division: ");
+            operation = Convert.ToChar(Console.ReadLine());
+
+
+        }
+    }
+}
